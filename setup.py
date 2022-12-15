1659ef8842169a7bd047bd571887f9fadf739df0
Shachaf Poran <shachafporan@Shachafs-MacBook-Pro.local>
2022-12-15 09:37:14 +0200

    Dist

--- setup.py
+++ setup.py
@@ -20,25 +20,31 @@ def gen_data_files(*dirs):
             results.append((root, map(lambda f:root + "/" + f, files)))
     return results
 
-
 setuptools.setup(
     name="skillner",
     version="0.0.1",
     author="Anas Ait AOMAR & Badr MOUFAD (Adapted by Shachaf Poran)",
-    author_email="Badr.MOUFAD@emines.um6p.ma",
-    description="An NLP module to automatically Extract skills and certifications from unstructured job postings, "
-                "texts, and applicant's resumes",
+    author_email="shachaf.p@goperfectmatch.com",
+    packages=setuptools.find_packages('.'),
+    package_dir={'': '.'},
     url="https://github.com/shachaf-perfect/skill_ner.git",
     keywords=["skillner", 'python', 'NLP', "NER", "skills-extraction", "job-description"],
-    # download_url='https://github.com/AnasAito/SkillNER/archive/refs/tags/v1.0.3.tar.gz',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    data_files=gen_data_files('.'),
-    package_dir={'': '.'},
     install_requires=dependencies,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        'License :: OSI Approved :: MIT License',
-        "Operating System :: OS Independent",
-    ],
 )
+
+
+# setup(
+#     name='talentfabric-core',
+#     version='0.4.0',
+#     license='MIT',
+#     author="Ariel Zadok",
+#     author_email='ariel.z@goperfectmatch.com',
+#     packages=find_packages('src'),
+#     package_dir={'': 'src'},
+#     url='https://github.com/TalentFabric/talentfabric-python-commons/talentfabric_core',
+#     keywords='talent id generator',
+#     install_requires=[
+#         'country-converter==0.7.4'
+#     ],
+#
+# )
